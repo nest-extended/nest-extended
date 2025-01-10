62443cda2b9ccff9deee73f74da821800127342d
Santanu <sahoosantanu92@gmail.com>
2025-01-11 00:01:16 +0530

    Core WIP

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,1 +1,26 @@
-export * from './lib/cli';
+import { Command } from 'commander';
+import {initializer} from "./initializer";
+// import { generateCode } from './code-generator';
+
+const program = new Command();
+
+program
+    .name('@nest-extended/cli')
+    .description('A CLI for generating NestJS code')
+    .version('0.0.1');
+
+program
+    .command('init')
+    .description('Create or update a nest-extended.config.json file')
+    .action(() => {
+        initializer();
+    });
+
+program
+    .command('g <type> <name>')
+    .description('Generate a NestJS component (e.g., module, controller, service)')
+    .action((type: string, name: string) => {
+        // generateCode(type, name);
+    });
+
+program.parse(process.argv);
---
+++ cli/src/initializer.ts
@@ -0,0 +1,5 @@
+import {initializeConfig} from "./config/config-init";
+
+export async function initializer() {
+    await initializeConfig();
+}
